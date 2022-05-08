2de139e35a8258903a8b7d516a6bda8ede09246f
prasam jain <90105063+pj1807@users.noreply.github.com>
2022-05-08 13:26:58 +0530

    Update main.py

--- Encryption & Decryption _txt-File/main.py
+++ Encryption & Decryption _txt-File/main.py
@@ -1,5 +1,9 @@
 # we use open() function with w+ mode to open file and override the existing content to perform encryption and decryption.
 
+f = open('text.txt','r')
+data = f.read()
+f.close()
+
 x = int(input("Press \n1 for encrypt\n2 for decrypt\n"))
 if x==1:
   f = open('text.txt','w+')
@@ -19,3 +23,16 @@ elif x==2:
   f.close()
 else:
   print("invalid choice !!")
+
+  
+  
+  
+  
+# ******** OUTPUT *********
+
+# open('text.txt')
+# File contains : Hello this is text.txt file for performing encryption and decryption.
+
+# ENCRYPTED FILE : ¦­­°aµ©ª´aª´aµ¦¹µoµ¹µa§ª­¦a§°³a±¦³§°³®ª¯¨a¦¯¤³º±µª°¯a¢¯¥a¥¦¤³º±µª°¯o
+
+# DECRYPTED FILE : Hello this is text.txt file for performing encryption and decryption.
